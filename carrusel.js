3124ff7cd9e1e9c4641eb9e6798eb5c2a2e0e0ec
xlourdes2001 <escuderolourdesmicaela@gmail.com>
2024-10-02 02:05:49 -0300

    formulario de contacto
    Se completó y finalizó el formulario de contacto con sus respectivas validaciones y se hicieron cambios en la estética de la pagina principal.

--- carrusel.js
+++ carrusel.js
@@ -22,3 +22,69 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Cambiar automáticamente la imagen cada 3 segundos, desactivado para ver si lo hacemos con botones
 //setInterval(() => changeImage(1), 3000); // Llama a changeImage cada 3 segundos
+
+//validacion de formulario
+
+function validateForm(){
+    let flagError = false;
+
+    let nombre = document.getElementById('nombre');
+    let email = document.getElementById('email');
+    let tel = document.getElementById('telefono');
+    let mensaje = document.getElementById('mensaje');
+
+    let namePattern = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+
+    if(nombre.value === "" || !namePattern.test(nombre.value)){
+        //verificamos si existe un span antes de crear una
+        let existingErrorSpan = nombre.nextElementSibling;
+        if(existingErrorSpan && existingErrorSpan.tagName ==='SPAN'){
+            existingErrorSpan.remove();
+        }
+
+        //creamos el span
+        let spanError = document.createElement('span');
+        spanError.appendChild(document.createTextNode("*Ingrese un nombre válido"));
+        nombre.insertAdjacentElement("afterend",spanError);
+        spanError.style.display= 'block';
+        flagError = true;
+    }
+
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(email.value === "" || !emailPattern.test(email.value)){
+        let existingErrorSpan = email.nextElementSibling;
+        if(existingErrorSpan && existingErrorSpan.tagName ==='SPAN'){
+            existingErrorSpan.remove();
+        }
+
+        let spanError = document.createElement('span');
+        spanError.appendChild(document.createTextNode("*Ingrese un email válido"));
+        email.insertAdjacentElement("afterend",spanError);
+        spanError.style.display= 'block';
+        flagError = true;
+    }
+
+    let telPattern = /^\+\d{1,3}\s\d{3}\s\d{7}$/;
+    if(tel.value === "" || !telPattern.test(tel.value)){
+        let existingErrorSpan = tel.nextElementSibling;
+        if(existingErrorSpan && existingErrorSpan.tagName ==='SPAN'){
+            existingErrorSpan.remove();
+        }
+
+        let spanError = document.createElement('span');
+        spanError.appendChild(document.createTextNode("*Ingrese un teléfono válido"));
+        tel.insertAdjacentElement("afterend",spanError);
+        spanError.style.display= 'block';
+        flagError = true;
+    }
+
+   
+    return !flagError;
+}
+
+function clearInput(e){
+    let spanError = e.target.nextElementSibling;  // Usar nextElementSibling en lugar de nextSibling
+    if(spanError.tagName === 'SPAN'){  // Asegurarse de que es un span
+        spanError.style.display = 'none';
+    }
+}
